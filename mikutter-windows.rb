76bfd5ffb39835ce75c7755be83a91011b168ab3
moguno <moguno@0kn.sakura.ne.jp>
2014-03-21 10:36:59 +0900

    Windowsのミキサーがmikutterアイコンで埋まる問題を回避。 正直Windowsさんが何とかする問題だと思います。

--- mikutter-windows.rb
+++ mikutter-windows.rb
@@ -75,6 +75,15 @@ end
 
 
 Plugin.create(:windows) do
+
+  # Windowsミキサーがmikutterアイコンで埋まる問題の対策
+  on_gui_window_change_icon { |i_window, icon|
+    window = Plugin[:gtk].widgetof(i_window)
+    if window
+      window.icon = Gdk::Pixbuf.new(icon, 32, 32)
+    end
+  }
+
   on_boot { |service|
     defactivity("windows", "Windowsプラグイン")
 
