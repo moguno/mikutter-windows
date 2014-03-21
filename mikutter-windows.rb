9503ebba344a500676f71f5de2f627f3bb53e5ab
moguno <moguno@0kn.sakura.ne.jp>
2014-03-21 21:33:22 +0900

    たまに変換出来ない文字がString::encode()に渡ってくるので"?"に置換することにする。

--- mikutter-windows.rb
+++ mikutter-windows.rb
@@ -28,6 +28,31 @@ def object_get_contents(fn)
 end
 
 
+# encode()のオプションを生成する
+def add_encode_options(options)
+  options[:invalid] = :replace
+  options[:undef] = :replace
+
+  options
+end
+
+
+class String
+  # 変換出来ない文字は全部"?"にしちゃえ！
+  alias :encode_org :encode
+  
+  def encode(encoding, options = {})
+    encode_org(encoding, add_encode_options(options))
+  end
+
+  alias :encode_org! :encode!
+
+  def encode!(encoding, options = {})
+    encode_org!(encoding, add_encode_options(options))
+  end
+end
+
+
 class Hash
   alias :inspect_org :inspect
 
