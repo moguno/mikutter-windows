0e4f7e14788f9ff694f7d7ba3c809ca1535433a4
moguno <moguno@0kn.sakura.ne.jp>
2015-01-20 23:02:05 +0900

    ツールチップを出したときにフリーズする事象を回避。

--- mikutter-windows.rb
+++ mikutter-windows.rb
@@ -87,6 +87,23 @@ end
 
 
 module Gtk
+  # ツールチップを出したときにフリーズする事象の回避
+  # ウィジェットのツールチップってTooltipsクラスが出すんだけど、意識してこいつの参照を保持しないとウィジェットより先にガベージコレクションされてしまう。
+  # それに伴ってGObjectが破棄されてるとするとすべての事象に説明が付くんだけど、再現コードでは問題なし。。。
+  # あと、ToolTipsはアプリで1インスタンスしか存在しなくても良いがmikutterは都度インスタンスを生成してるので、それがWindowsではギルティなのかもしれない。
+  # 取りあえずシングルトンっぽく1つのインスタンスを共有する様にしたら落ちなくなったからまぁいいや。今日もいい天気。
+  class Tooltips
+    class << self
+      @@unique_tips = Gtk::Tooltips.new
+
+      alias :new_org new
+
+      def new
+        @@unique_tips
+      end
+    end
+  end
+
   class << self
     alias :openurl_org :openurl
 
