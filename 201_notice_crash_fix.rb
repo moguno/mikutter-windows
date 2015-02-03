3b6b89148aec39ab72a72a3208de419b27e3fd2e
moguno <moguno@0kn.sakura.ne.jp>
2015-02-03 23:34:34 +0900

    日本語名の抽出タブを編集しようとすると落ちる問題を解決

--- 201_notice_crash_fix.rb
+++ 201_notice_crash_fix.rb
@@ -1,18 +1,22 @@
 # noticeでDOS窓にUTF-8を表示しようとして落ちる「魔の141行目問題」を回避するよ。
 
+class String
+  def inspect()
+    encode(Encoding.default_external)
+  rescue => e
+    "(?????)"
+  end
+end
+
 class Hash
   alias :inspect_org :inspect
 
   def inspect()
     result = self.inject({}) { |result, (key, item)|
-      if item.is_a?(String)
-        begin
-          result[key] = item.encode(Encoding.default_external)
-        rescue => e
-          result[key] = "(?????)"
-        end
+      result[key] = if item.is_a?(String)
+        item.inspect
       else
-        result[key] = item
+        item
       end
 
       result
