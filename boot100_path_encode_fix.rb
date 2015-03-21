7ff38c0ae991d26ee83edba68988a82f654553b1
moguno <sns@0kn.sakura.ne.jp>
2015-03-21 21:31:06 +0900

    File.expand_pathに文字コード変換を追加

--- boot100_path_encode_fix.rb
+++ boot100_path_encode_fix.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# File.joinの結果がWindows-31Jになって各種ファイル操作に失敗するのをFix
+# パスを格納した文字列がWindows-31Jになって各種ファイル操作に失敗するのをFix
 class Dir
   class << self
     alias :pwd_org :pwd
@@ -17,6 +17,28 @@ class File
 
     def join(*args)
       join_org(*args).encode("UTF-8")
+    end
+
+    alias :expand_path_org :expand_path
+
+    def expand_path(*args)
+      expand_path_org(*args).encode("UTF-8")
+    end
+  end
+end
+
+class Exception
+  alias :backtrace_org :backtrace
+
+  def backtrace
+    bt = backtrace_org
+
+    if bt
+      bt.map {|_|
+        _.encode("UTF-8")
+      }
+    else
+      bt
     end
   end
 end
