2a50d0cb7bd221382ea2b2b0bb58a9055e6f2e6f
moguno <sns@0kn.sakura.ne.jp>
2016-10-19 22:57:24 +0900

    gdk_pixbuf 3.0.9のメソッドの呼び合いに失敗してるのをFix

---
+++ 450_gdk_pixbuf_3_0_9_function_name_fix.rb
@@ -0,0 +1,7 @@
+# GdkPixbuf 3.0.9でいくつかのメソッドの名前が変わってるのを修正
+module GdkPixbuf
+  class Pixbuf
+    alias :initialize_new_from_file_at_size :initialize_new_from_file_at_size_utf8
+    alias :initialize_new_from_file_at_scale :initialize_new_from_file_at_scale_utf8
+  end
+end
