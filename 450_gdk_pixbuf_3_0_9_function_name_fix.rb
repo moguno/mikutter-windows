5e1fdff124b1b842b10ee42cb221ce17acec6122
valda <valda@underscore.jp>
2016-12-11 15:08:10 +0900

    GdkPixbuf　3.0.9で変更されたメソッド名の有無を確認してからaliasするようにしました

--- 450_gdk_pixbuf_3_0_9_function_name_fix.rb
+++ 450_gdk_pixbuf_3_0_9_function_name_fix.rb
@@ -1,8 +1,17 @@
 # GdkPixbuf 3.0.9でいくつかのメソッドの名前が変わってるのを修正
 module GdkPixbuf
   class Pixbuf
-    alias :initialize_new_from_file_at_size :initialize_new_from_file_at_size_utf8
-    alias :initialize_new_from_file_at_scale :initialize_new_from_file_at_scale_utf8
-    alias :initialize_new_from_file :initialize_new_from_file_utf8
+    if private_method_defined?(:initialize_new_from_file_utf8)
+      alias_method :initialize_new_from_file,
+                   :initialize_new_from_file_utf8
+    end
+    if private_method_defined?(:initialize_new_from_file_at_size_utf8)
+      alias_method :initialize_new_from_file_at_size,
+                   :initialize_new_from_file_at_size_utf8
+    end
+    if private_method_defined?(:initialize_new_from_file_at_scale_utf8)
+      alias_method :initialize_new_from_file_at_scale,
+                   :initialize_new_from_file_at_scale_utf8
+    end
   end
 end
